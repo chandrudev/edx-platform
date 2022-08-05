c11fb03facaebffcdeb1d09832d56da9dad57d63
shiv <shivam.sharma@synoriq.in>
2022-08-05 14:17:14 +0530

    delete-button

--- cms/static/js/views/modals/course_outline_modals.js
+++ cms/static/js/views/modals/course_outline_modals.js
@@ -284,12 +284,14 @@
             let fileItem = document.createElement("li");
             fileItem.innerHTML = item.document_name;
             fileItem.key = item.id;
+            fileItem.style="border-bottom: 1px solid #dddddd, padding-bottom: 4px;";
 
             let deleteFile = document.createElement("button");
             deleteFile.innerText = "Delete";
+            deleteFile.style="float:right";
             deleteFile.onclick = () =>{
                 const endpoint = "../delete_doc";              
-                $.postJSON(endpoint, item.id, function (data) {alert("Document Deleted Successfully")});
+                $.postJSON(endpoint, {"doc_id":item.id}, function (data) {alert("Document Deleted Successfully")});
             }
             fileItem.appendChild(deleteFile);
 
