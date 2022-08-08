ef271ddcd31eb85cd9595f1d06c624847272e84c
shiv <shivam.sharma@synoriq.in>
2022-08-08 18:29:40 +0530

    reverted-toggle

--- lms/djangoapps/course_home_api/toggles.py
+++ lms/djangoapps/course_home_api/toggles.py
@@ -2,14 +2,12 @@
 Toggles for course home experience.
 """
 
-from edx_toggles.toggles import LegacyWaffleFlagNamespace
 
 from openedx.core.djangoapps.waffle_utils import CourseWaffleFlag
 
-WAFFLE_FLAG_NAMESPACE = LegacyWaffleFlagNamespace(name='course_home')
+WAFFLE_FLAG_NAMESPACE ='course_home'
 
-COURSE_HOME_MICROFRONTEND_PROGRESS_TAB = CourseWaffleFlag(WAFFLE_FLAG_NAMESPACE, 'course_home_mfe_progress_tab',  # lint-amnesty, pylint: disable=toggle-missing-annotation
-                                                          __name__)
+
 
 # .. toggle_name: course_home.course_home_use_legacy_frontend
 # .. toggle_implementation: CourseWaffleFlag
@@ -29,11 +27,4 @@ def course_home_legacy_is_active(course_key):
     return COURSE_HOME_USE_LEGACY_FRONTEND.is_enabled(course_key) or course_key.deprecated
 
 
-def course_home_mfe_progress_tab_is_active(course_key):
-    # Avoiding a circular dependency
-    from .models import DisableProgressPageStackedConfig
-    return (
-        (not course_home_legacy_is_active(course_key)) and
-        COURSE_HOME_MICROFRONTEND_PROGRESS_TAB.is_enabled(course_key) and
-        not DisableProgressPageStackedConfig.current(course_key=course_key).disabled
     )
